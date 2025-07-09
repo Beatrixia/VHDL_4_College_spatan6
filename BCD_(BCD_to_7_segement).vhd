e5743a178d9cc21ef7115b33d128cb3eefb569a9
Beatrixia <tanarukbright@gmail.com>
2025-07-09 15:42:25 +0700

    Update BCD_(BCD_to_7_segement).vhd

--- BCD_(BCD_to_7_segement).vhd
+++ BCD_(BCD_to_7_segement).vhd
@@ -23,33 +23,30 @@ begin
 
 	Seven_Segment : process(Cnt_in)
 	begin
-		if (En = '1') then
-			case Cnt_in is
-				-- 7_segment (DP)GFEDCBA
-				when "0000" => tmp <= "00111111";	-- 0
-				when "0001" => tmp <= "00000110";	-- 1
-				when "0010" => tmp <= "01011011";	-- 2
-				when "0011" => tmp <= "01001111";	-- 3
-				when "0100" => tmp <= "01100110";	-- 4
-				when "0101" => tmp <= "01101101";	-- 5
-				when "0110" => tmp <= "01111101";	-- 6
-				when "0111" => tmp <= "00100111";	-- 7
-				when "1000" => tmp <= "01111111";	-- 8
-				when "1001" => tmp <= "01101111";	-- 9
-				when "1010" => tmp <= "01110111";	-- A
-				when "1011" => tmp <= "01111100";	-- b
-				when "1100" => tmp <= "00111001";	-- C
-				when "1101" => tmp <= "01101111";	-- d
-				when "1110" => tmp <= "01111001";	-- E
-				when others => tmp <= "01110001";	-- F
-			-- the meaning in this when part
-			-- when "Cnt_in" => tmp <= "The vector that we want to set it to tmp";
-			-- in tmp vector in this case(not the case fungtion) 1 is led on (1n 7 segment) 0 is led off (in 7 segment)
-			
-			end case;
-		else
-			tmp <= "00000000";
-		end if;
+		case Cnt_in is
+			-- 7_segment (DP)GFEDCBA
+			when "0000" => tmp <= "00111111";	-- 0
+			when "0001" => tmp <= "00000110";	-- 1
+			when "0010" => tmp <= "01011011";	-- 2
+			when "0011" => tmp <= "01001111";	-- 3
+			when "0100" => tmp <= "01100110";	-- 4
+			when "0101" => tmp <= "01101101";	-- 5
+			when "0110" => tmp <= "01111101";	-- 6
+			when "0111" => tmp <= "00100111";	-- 7
+			when "1000" => tmp <= "01111111";	-- 8
+			when "1001" => tmp <= "01101111";	-- 9
+			when "1010" => tmp <= "01110111";	-- A
+			when "1011" => tmp <= "01111100";	-- b
+			when "1100" => tmp <= "00111001";	-- C
+			when "1101" => tmp <= "01101111";	-- d
+			when "1110" => tmp <= "01111001";	-- E
+			when others => tmp <= "01110001";	-- F
+		-- the meaning in this when part
+		-- when "Cnt_in" => tmp <= "The vector that we want to set it to tmp";
+		-- in tmp vector in this case(not the case fungtion) 1 is led on (1n 7 segment) 0 is led off (in 7 segment)
+		
+		end case;
+
 	end process Seven_Segment;
 	
 	Cnt_out <= tmp;
