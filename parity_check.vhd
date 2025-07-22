c367e2ecb4484a869dc4a39e9c258b490f04609d
Beatrixia <tanarukbright@gmail.com>
2025-07-22 17:15:31 +0700

    Update parity_check.vhd

--- parity_check.vhd
+++ parity_check.vhd
@@ -1,17 +1,20 @@
 ----------------------------------------------------------------------------------
+-- Create Date:    16:58:47 07/22/2025 
+-- Module Name:    parity - Behavioral 
+----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity flash is
-    port ( inputs,clk : in STD_LOGIC;
+entity parity is
+    port ( inputs, clk, rst : in STD_LOGIC;
            outputs : out STD_LOGIC );
-end flash;
+end parity;
 
-architecture Behavioral of flash is
+architecture Behavioral of parity is
     
     type state_type is (s0 ,s1);
-    signal state,next_state : state_type
-    signal parity : STD_LOGIC;
+    signal state,next_state : state_type;
+    signal paritys : STD_LOGIC;
 
 begin
 
@@ -26,27 +29,27 @@ begin
         end if;
     end process proc;
 
-    output_decode : process (state)
+    output_decode : process (state, inputs)
     begin
         case (state) is 
             when s0 => 
                 if (inputs = '0') then
-                    parity <= '0';
+                    paritys <= '0';
                 else
-                    parity <= '1';
+                    paritys <= '1';
                 end if;
             when s1 => 
                 if (inputs = '0') then
-                    parity <= '1';
+                    paritys <= '1';
                 else
-                    parity <= '0';
+                    paritys <= '0';
                 end if;
             when others =>
-                parity <= '0';
+                paritys <= '0';
         end case;
     end process output_decode;
 
-    next_state_decode : process (state,inputs)
+    next_state_decode : process (state, inputs, next_state)
     begin
         case (state) is
             when s0 =>
@@ -57,15 +60,15 @@ begin
                 end if;
             when s1 =>
                 if (inputs = '0') then
-                    next_state <= s0;
-                else
                     next_state <= s1;
+                else
+                    next_state <= s0;
                 end if;
             when others => 
                 next_state <= next_state;
         end case;
     end process next_state_decode;
 
-    outputs <= parity;
+    outputs <= paritys;
     
 end architecture Behavioral;
